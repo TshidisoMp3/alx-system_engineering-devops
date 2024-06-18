89eff129f597ef13dccd6e45b66f5f2f89a413f9
TshidisoMp3 <tshidisomorake18@gmail.com>
2024-06-18 14:18:39 +0200

    API Advanced

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -2,3 +2,15 @@
 """Function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit"""
 
 import requests
+
+
+def top_ten(subreddit):
+    """Function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit"""
+    headers = {'User-agent': 'test'}
+    res = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
+                       .format(subreddit), headers=headers)
+    try:
+        for i in range(10):
+            print(res.json()['data']['children'][i]['data']['title'])
+    except Exception:
+        print(None)
