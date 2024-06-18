25ce7561714ed37a58e68673632cf34568be64be
TshidisoMp3 <tshidisomorake18@gmail.com>
2024-06-18 14:13:27 +0200

    API Advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,13 +7,13 @@ module for the Reddit API
 import requests
 
 
-def number_of_subscribers(subscribers):
+def number_of_subscribers(subreddit):
     '''function that queries and returns the number of subscribers '''
     headers = {'User-agent': 'test'}
     res = requests.get("https://www.reddit.com/r/{}/about.json"
-                       .format(subscribers), headers=headers)
+                       .format(subreddit), headers=headers)
     try:
-        subs = res.json()['data']['subscribers']
+        subsreds = res.json()['data']['subscribers']
     except Exception:
         return 0
-    return subs
+    return subsreds
