152a07629ebbbaa54b3ff631e784fd08794097c5
TshidisoMp3 <tshidisomorake18@gmail.com>
2024-06-18 13:34:34 +0200

    API Advanced

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Function that queries the Reddit API and returns the number of subscribers"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Function that queries the Reddit API and returns the number of subscribers"""
+    headears = {'User-Agent': 'Mozilla/5.0'}
+    resp = requests.get('https://www.reddit.com/r/{}/about.json'.format(subreddit),
+                        headers=headears)
+    if resp.status_code == 200:
+        return resp.json().get('data').get('subscribers')
+    return 0
