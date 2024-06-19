90a654b084b6cb40713c5b1397f0f422d005b9e0
TshidisoMp3 <tshidisomorake18@gmail.com>
2024-06-19 20:21:23 +0200

    Api_Advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-'''module for the Reddit API'''
+"""module for the Reddit API"""
+
 import requests
 
 
@@ -7,9 +8,10 @@ def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
-
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
